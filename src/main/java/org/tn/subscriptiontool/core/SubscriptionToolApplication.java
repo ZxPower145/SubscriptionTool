bec84eaa49f9191a0bcdc96e4433d719f1458db9
ZxPower <zxpower@yahoo.com>
2024-10-21 19:15:50 +0300

    Minor changes

--- src/main/java/org/tn/subscriptiontool/core/SubscriptionToolApplication.java
+++ src/main/java/org/tn/subscriptiontool/core/SubscriptionToolApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.tn.subscriptiontool.core.security.models.Role;
 import org.tn.subscriptiontool.core.security.repository.RoleRepository;
 
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SubscriptionToolApplication {
 
     @Bean
